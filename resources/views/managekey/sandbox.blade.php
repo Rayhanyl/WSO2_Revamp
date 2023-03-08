8084ba53657d5541057ffd868a7c492c9082ea10
rayhanyl <rayhanyuda13@gmail.com>
2023-03-08 14:38:59 +0700

    Fix clipboard

--- resources/views/managekey/sandbox.blade.php
+++ resources/views/managekey/sandbox.blade.php
@@ -431,7 +431,9 @@
                     <div>
                         <small>Above token has a validity period of seconds.</small>
                     </div>
-                    <button class="btn btn-success" onclick="copyApikey()">Copy To clipboard</button>
+                    {{-- <button class="btn btn-success" onclick="copyApikey()">Copy To clipboard</button> --}}
+                    <button class="btn btn-success" onClick="copyApikey()">Copy To clipboard</button>
+                    {{-- <button onClick="copyApikey()">➡️ Copy Message to Clipboard</button> --}}
                 </div>
             </div>
             <div class="modal-footer">
@@ -448,6 +450,51 @@
 
 @push('script')
 <script>
+
+    const unsecuredCopyToClipboard = (text) => { 
+        const textArea = document.createElement("textarea"); 
+        textArea.value=text; 
+        document.body.appendChild(textArea);                 
+        textArea.focus();
+        textArea.select(); 
+        try
+            {
+            document.execCommand('copy')
+            }catch(err){
+                Swal.fire(
+                'Error',
+                '',
+                'warning'
+                )
+            }
+            document.body.removeChild(textArea)};
+
+    const copyToClipboard = (content) => {
+        if (window.isSecureContext && navigator.clipboard) {
+            navigator.clipboard.writeText(content);
+            Swal.fire(
+            'Already Copied',
+            '',
+            'success'
+            ) 
+            console.log('Hellooo'); 
+        } else {
+            unsecuredCopyToClipboard(content);
+            Swal.fire(
+            'Already Copied',
+            '',
+            'success'
+            )  
+        }
+    };
+    var resultApikey = document.getElementById("text-apikey");
+    const copyApikey = () => { 
+        copyToClipboard(resultApikey.value);   
+    };
+    var consumerKey = document.getElementById("consumerkey");
+    const copyConsumerKey = () => { 
+        copyToClipboard(consumerKey.value);   
+    };
 
     $(document).ready(function () {
         $('.result-accesstoken').hide();
@@ -523,21 +570,6 @@
         }
     });
 
-    function copyConsumerKey() {
-        var copyText = document.getElementById("consumerkey");
-
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); // For mobile devices
-
-        navigator.clipboard.writeText(copyText.value);
-
-        Swal.fire(
-            'Already Copied',
-            '',
-            'success'
-        )
-    }
-
     function copySecretKey() {
         var copyText = document.getElementById("secretkey");
 
@@ -580,12 +612,9 @@
 
     function copyTokenEndpoint() {
         var copyText = document.getElementById("tokenendpoint");
-
         copyText.select();
         copyText.setSelectionRange(0, 99999); // For mobile devices
-
         navigator.clipboard.writeText(copyText.value);
-
         Swal.fire(
             'Already Copied',
             '',
@@ -764,27 +793,6 @@
                 }
             });
         });
-
-        // Copy text APIKEY
-        function copyApikey() {
-            // Get the text field
-            var copyText = document.getElementById("text-apikey");
-
-            // Select the text field
-            copyText.select();
-            copyText.setSelectionRange(0, 99999); // For mobile devices
-
-            // Copy the text inside the text field
-            navigator.clipboard.writeText(copyText.value);
-            // if(copyText.value.length > 10) copyText.value = copyText.value.substring(0,20);
-
-            Swal.fire(
-            'Already Copied',
-            '',
-            'success'
-            )
-        }
-
 </script>
 @endpush
 
